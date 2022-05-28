0be3e85826a3f251aaba9e5ce1dcbc4465297ba4
Craig Edwards <jamesallyncraigedwards@gmail.com>
2022-05-28 10:43:29 -0400

    LoginPostReqNoWorkHeadingBackToDatabaseP2

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,19 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from .authforms import LoginForm
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates', url_prefix='/auth')
 
-#commented below out when had problems importing flask_wtf
-# from .authforms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    # return 'test'
- #commented out below when having problems with flask_wtf forms.   
+   
     lform = LoginForm()
+###Stopped here in the forms video
+###because the if clause was not working
+###When filled in login form and hit submit, did not get the "thanks for logging in message below."
+    if request.method == 'POST':
+        print('form submitted')
+        return 'Thanks for logging in.'
+
+
     return render_template('signin.html', form = lform)
 
-@auth.route('/base')
-def base_test():
+# @auth.route('/base')
+# def base_test():
 
-    return render_template('base.html')
+#     return render_template('base.html')
