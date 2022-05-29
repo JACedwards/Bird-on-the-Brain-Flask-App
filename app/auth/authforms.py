42cb25c454a3f500ed06fc6795b88476cf718772
Craig Edwards <jamesallyncraigedwards@gmail.com>
2022-05-29 09:03:33 -0400

    RightBeforeStartingRegister.html

--- app/auth/authforms.py
+++ app/auth/authforms.py
@@ -1,9 +1,19 @@
+from ast import Pass
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 
 class LoginForm(FlaskForm):
    username = StringField('Username', validators=[DataRequired()])
    password = PasswordField('Password', validators=[DataRequired()])
    submit = SubmitField()
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[EqualTo('password')])
+    submit = SubmitField()
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,23 +1,37 @@
-from flask import Blueprint, render_template, request
-from .authforms import LoginForm
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .authforms import LoginForm, RegistrationForm
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates', url_prefix='/auth')
 
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
-
    
     lform = LoginForm()
-###Stopped here in the forms video
-###because the if clause was not working
-###When filled in login form and hit submit, did not get the "thanks for logging in message below."
+
     if request.method == 'POST':
-        print('form submitted')
-        return 'Thanks for logging in.'
-
-
+        if lform.validate_on_submit():
+           username = lform.username.data
+           password = lform.password.data
+           print('formdata:', username, password)
+           flash(f'Success -- You have been signed in, {username}.', 'success')
+           return redirect(url_for('home'))
+        else:
+            return redirect(url_for('auth.login'))
+        
     return render_template('signin.html', form = lform)
 
+@auth.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            print(form.data)
+            flash('Welcome! Thank you for registering!', 'info')
+            return redirect(url_for('home'))
+        else:
+            flash('Sorry, that username or email is taken.  Please try again.', 'danger')
+            return redirect(url_for('auth.register'))
 
+    return render_template('register.html', form=form)
 
